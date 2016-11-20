26ac52d468db688d2b864d75f83dd5f33be118df
sandip80 <anpham2@illinois.edu>
2016-11-20 00:08:16 -0600

    Add recticle. Remove unnecessary assets.

---
+++ Assets/reticle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reticle : MonoBehaviour {
+    public Camera CameraFacing;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = CameraFacing.transform.position
+         //   + CameraFacing.transform.rotation * Vector3.forward;
+        //transform.LookAt(CameraFacing.transform.position);
+        //transform.Rotate(0.0f, 180.0f, 0.0f);
+	}
+}
