cc41c3758a197511db95feeed2403f85684a47b2
sandip80 <anpham2@illinois.edu>
2016-11-20 00:54:40 -0600

    Add scale, rotate and transform features to Globe

--- Assets/Custom Script/HandleEarth.cs
+++ Assets/Custom Script/HandleEarth.cs
@@ -12,6 +12,28 @@ public class HandleEarth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+	    if (Input.GetButtonDown("u"))
+        {
+            transform.localScale += new Vector3(0.03f, 0.03f, 0.03f);
+        }
+        if (Input.GetButtonDown("y"))
+        {
+            transform.localScale -= new Vector3(0.03f, 0.03f, 0.03f);
+        }
+        if (Input.GetButtonDown("a")) {
+            transform.position += new Vector3(0.0f, 0.3f, 0.0f);
+        }
+        if (Input.GetButtonDown("b"))
+        {
+            transform.position -= new Vector3(0.0f, 0.3f, 0.0f);
+        }
+        if (Input.GetButtonDown("o"))
+        {
+            transform.Rotate(new Vector3(0.0f, 10.0f, 0.0f));
+        }
+        if (Input.GetButtonDown("p"))
+        {
+            transform.Rotate(new Vector3(0.0f, -10.0f, 0.0f));
+        }
+    }
 }
